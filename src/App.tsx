4d7b251b7b6ca2a565e2d0dbf8e1c27002e4d53f
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2023-08-15 15:04:29 +0200

    reverse geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import { getPosition } from './getPosition/getPosition'
+import { getPosition, reverseGeocode } from './getPosition/getPosition'
 
 function App() {
 
 
   const [message, setMessage] = useState<string>('')
+  const [address, setAddress] = useState<string>('')
+
+  const lat = 57.700735, lon = 11.9551032
 
   return (
-    
+
     <div className='vertical-layout'>
 
       <header>
@@ -18,6 +21,10 @@ function App() {
       <main>
         <button onClick={ () => getPosition(setMessage) }>Get Location!</button>
         <p>{message}</p>
+
+        <button onClick={ () => reverseGeocode(lat, lon, setAddress)}>Get City!</button>
+        <p>{address}</p>
+
       </main>
 
     </div>
--- src/getPosition/GetPosition.tsx
+++ src/getPosition/GetPosition.tsx
@@ -13,14 +13,34 @@ function getPosition(setMessage: ReactSetState<string>) {
   
         //   console.log('position is: ', position)
           setMessage(`Your position is:  ${position.coords.latitude} latitude and ${position.coords.longitude} longitude`)
-        }, error => {
+        }, () => {
         //   console.log('position error', error)
           setMessage('Please enable position to use this app.')
         })
       }
     }
 
+    const apiKey: string = 'f551cddf89208d38b63c2f351454dc12'
+
+    
+    async function reverseGeocode(lat: number, lon:number, setAdress: ReactSetState<string>){
+        
+        const numberOfResponses = 5
+        const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${numberOfResponses}&appid=${apiKey}`
+        const response = await fetch(url)
+        const data: Place[] = await response.json()
+
+        console.log('Reverse geocode: ', data);
+
+        let firstAddress: string = data[0].name
+        setAdress(firstAddress)
+        
+
+        interface Place{
+            name: string;
+        }
+    }
 
 
 
-export {getPosition};
+export {getPosition, reverseGeocode};
